5ff7bf27e756dd77ee41e9e7b078609b451c96ab
Abbos Shirinboev <abbosboev@gmail.com>
2022-05-24 14:51:40 -0400

    Step-1 is done.

--- src/main/java/com/example/accountingapp/entity/Category.java
+++ src/main/java/com/example/accountingapp/entity/Category.java
@@ -1,14 +1,12 @@
 package com.example.accountingapp.entity;
 
+import com.example.accountingapp.enums.Unit;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Where;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.math.BigInteger;
 import java.time.LocalDate;
 
@@ -17,17 +15,15 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @Where(clause = "is_deleted=false")
-public class Category {
+public class Category extends BaseEntity{
 
     private String description;
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    private Company company;
+    @Enumerated(EnumType.STRING)
+    private Unit unit;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Company company;
     private boolean enabled;
     private BigInteger created_by;
     private BigInteger updated_by;
-//    @Column(columnDefinition = "DATE")
-//    private LocalDate created_time;
-//
-//    @Column(columnDefinition = "DATE")
-//    private LocalDate updated_time;
+
 }
