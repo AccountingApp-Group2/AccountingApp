63977192d0d812e5382ef576a3c9281528ce0dc7
Abbos Shirinboev <abbosboev@gmail.com>
2022-05-24 14:51:40 -0400

    Step-1 is done.

--- src/main/java/com/example/accountingapp/enums/Unit.java
+++ src/main/java/com/example/accountingapp/enums/Unit.java
@@ -1,4 +1,13 @@
 package com.example.accountingapp.enums;
 
 public enum Unit {
+    PIECES("Pieces");
+
+    private final String value;
+    Unit(String value) {
+        this.value = value;
+    }
+    public String getValue() {
+        return value;
+    }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,8 @@ VALUES (1, '2022-01-05 00:00:00', FALSE, 1, '2022-01-05 00:00:00', 'name1', 100,
        (2, '2022-01-05 00:00:00', FALSE, 1, '2022-01-05 00:00:00', 'name2', 200, 15, 2, 8, 2),
        (3, '2022-01-05 00:00:00', FALSE, 1, '2022-01-05 00:00:00', 'name3', 300, 20, 3, 10, 3);
 
+--TODO @Abbos - category
+INSERT INTO invoice_product (created_by, created_time, is_deleted, updated_by, updated_time, description, enabled)
+VALUES (1, '2022-01-05 00:00:00', FALSE, 1, '2022-01-05 00:00:00', 'computer', true),
+       (2, '2022-01-05 00:00:00', FALSE, 1, '2022-01-05 00:00:00', 'TV', true ),
+       (3, '2022-01-05 00:00:00', FALSE, 1, '2022-01-05 00:00:00', 'sports', true);
