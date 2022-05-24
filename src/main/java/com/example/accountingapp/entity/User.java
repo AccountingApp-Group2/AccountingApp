e5f2e28a9e6a58b15e532dcc35b5ad637aa4922f
RumCake01 <mia.hasnutdinova@gmail.com>
2022-05-24 08:56:13 -0400

    User entity and State enum added. TODOs added for Muhabbat and Gulmira

---
+++ src/main/java/com/example/accountingapp/entity/User.java
@@ -0,0 +1,35 @@
+package com.example.accountingapp.entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+@Where(clause= "is_deleted=false")
+public class User extends BaseEntity {
+
+    private String firstName;
+    private String LastName;
+    @Column(unique = true, nullable = false)
+    private String userName;
+    private String password;
+    private String phone;
+    private String status;
+
+    // ToDo Gulmira
+   /* @ManyToOne(fetch = FetchType.LAZY)
+    private Company company;
+    // ToDo Muhabbat
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Role role;*/
+
+
+}
+
+
