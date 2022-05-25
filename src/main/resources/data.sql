3285c444a42eabd89d653030ce6cead603c9612b
RumCake01 <mia.hasnutdinova@gmail.com>
2022-05-24 23:34:43 -0400

    Added data in data.sql for testing the User entity + added TODOs

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -21,3 +21,10 @@ INSERT INTO category (created_by, created_time, is_deleted, updated_by, updated_
 VALUES (1, '2022-01-05 00:00:00', FALSE, 1, '2022-01-05 00:00:00', 'computer', true),
        (2, '2022-01-05 00:00:00', FALSE, 1, '2022-01-05 00:00:00', 'TV', true ),
        (3, '2022-01-05 00:00:00', FALSE, 1, '2022-01-05 00:00:00', 'sports', true);
+
+
+INSERT INTO users(created_by, created_time, updated_by, updated_time, first_name, last_name, email, password, phone, enabled, is_deleted)
+    --TODO Gulmira and Muhabbat need to create below entities - company_id, role_id)
+VALUES (10, '2022-01-05 00:12:00', 10, '2022-01-05 00:20:00', 'Marylin', 'Monro', 'monro@gmail.com', 'monro10', '2063334611', TRUE, FALSE),
+       (20, '2022-01-05 00:12:00', 20, '2022-01-05 00:20:00', 'Michael', 'Jackson', 'jackson01@gmail.com', 'jackson10', '2063334612', TRUE, FALSE),
+       (30, '2022-01-05 00:12:00', 30, '2022-01-05 00:20:00', 'Paul', 'Smith', 'smith@gmail.com', 'smith29', '2063334613', TRUE, FALSE);
