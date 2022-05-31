0307ae13e81436e505074bbf9385d810458d1c9b
Mehmet <mathdenizi@gmail.com>
2022-05-31 19:32:43 +0200

    User can be save with role , company  and status , all work properly...

--- src/main/java/com/example/accountingapp/controller/LoginController.java
+++ src/main/java/com/example/accountingapp/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.accountingapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 
@@ -9,9 +10,14 @@ public class LoginController {
 
 
     @GetMapping(value = {"/", "/login"})
-    public String loginM() {
+    public String login() {
         return "login";
     }
 
+    @RequestMapping("/main2")
+    public String main2() {
+        return "main2";
+    }
 
 }
+
