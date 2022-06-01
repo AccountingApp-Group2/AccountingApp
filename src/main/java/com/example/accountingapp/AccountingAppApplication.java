da837c39809923c6e3cc93d1c43e9cd51ed77a67
josephum <39348176+josephum@users.noreply.github.com>
2022-06-01 13:34:18 -0400

    Update AccountingAppApplication.java

--- src/main/java/com/example/accountingapp/AccountingAppApplication.java
+++ src/main/java/com/example/accountingapp/AccountingAppApplication.java
@@ -11,6 +11,7 @@ public class AccountingAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountingAppApplication.class, args);
     }
+    
     @Bean
     public ModelMapper mapper(){
         return new ModelMapper();
