3de11bd00fb0dc5d5fdf97c75b1f5e4f9bde7abb
VitalySlabada <vitaly.slabada@gmail.com>
2022-06-01 23:57:41 -0500

    invoiceList start

--- src/main/java/com/example/accountingapp/service/Impl/InvoiceServiceImplemetation.java
+++ src/main/java/com/example/accountingapp/service/Impl/InvoiceServiceImplemetation.java
@@ -1,5 +1,6 @@
 package com.example.accountingapp.service.Impl;
 
+import com.example.accountingapp.dto.InvoiceDTO;
 import com.example.accountingapp.enums.InvoiceType;
 import com.example.accountingapp.service.InvoiceService;
 import org.springframework.stereotype.Service;
@@ -9,7 +10,7 @@ import java.util.List;
 @Service
 public class InvoiceServiceImplemetation implements InvoiceService {
     @Override
-    public List<InvoiceDto> listAllByInvoiceType(InvoiceType invoiceType) {
+    public List<InvoiceDTO> listAllByInvoiceType(InvoiceType invoiceType) {
         return null;
     }
 }
