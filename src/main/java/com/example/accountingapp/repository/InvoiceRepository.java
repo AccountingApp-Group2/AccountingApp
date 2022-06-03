231c3cf8970904310e382dd544eb6437915adb32
BahaMaksudov <bmaksudov@gmail.com>
2022-06-02 23:19:37 -0400

    created InvoiceServImpl

--- src/main/java/com/example/accountingapp/repository/InvoiceRepository.java
+++ src/main/java/com/example/accountingapp/repository/InvoiceRepository.java
@@ -10,4 +10,8 @@ public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 
     List<Invoice> findAllByInvoiceType(InvoiceType invoiceType);
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> cb0d794 (created InvoiceServImpl)
 }
