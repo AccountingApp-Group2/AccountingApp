589c22b3b74cf48a8945a3c3e1d79d14e63bc07e
VitalySlabada <vitaly.slabada@gmail.com>
2022-06-05 18:26:29 -0500

    step 2 done for purchase invoice + cleanup

--- src/main/java/com/example/accountingapp/service/Impl/InvoiceServiceImpl.java
+++ src/main/java/com/example/accountingapp/service/Impl/InvoiceServiceImpl.java
@@ -10,8 +10,6 @@ import com.example.accountingapp.service.InvoiceService;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
-import java.math.RoundingMode;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,9 +30,7 @@ public class InvoiceServiceImpl implements InvoiceService {
     @Override
     public List<InvoiceDTO> listAllByInvoiceType(InvoiceType invoiceType) {
 
-        List<InvoiceDTO> listDTO = invoiceRepository.findAllByInvoiceType(invoiceType).stream()
-                .map(p -> mapperUtil.convert(p, new InvoiceDTO()))
-                .collect(Collectors.toList());
+        List<InvoiceDTO> listDTO = invoiceRepository.findAllByInvoiceType(invoiceType).stream().map(p -> mapperUtil.convert(p, new InvoiceDTO())).collect(Collectors.toList());
 
         //set cost
         listDTO.forEach(p -> p.setCost((calculateCostByInvoiceID(p.getId())).setScale(2)));
@@ -43,7 +39,7 @@ public class InvoiceServiceImpl implements InvoiceService {
         listDTO.forEach(p -> p.setTax(((p.getCost().multiply(BigDecimal.valueOf(0.01)))).setScale(2)));
 
         //set total
-        listDTO.forEach(p -> p.setTotal((p.getCost())));
+        listDTO.forEach(p -> p.setTotal((p.getCost()).add(p.getTax())));
         return listDTO;
     }
 
@@ -51,12 +47,10 @@ public class InvoiceServiceImpl implements InvoiceService {
     public BigDecimal calculateCostByInvoiceID(Long id) {
 
         //Get list of all invoice-products by invoice ID
-        List<InvoiceProductDTO> invoiceProductListById = invoiceProductRepository.findAllByInvoiceId(id).stream()
-                .map(p -> mapperUtil.convert(p, new InvoiceProductDTO()))
-                .collect(Collectors.toList());
+        List<InvoiceProductDTO> invoiceProductListById = invoiceProductRepository.findAllByInvoiceId(id).stream().map(p -> mapperUtil.convert(p, new InvoiceProductDTO())).collect(Collectors.toList());
         BigDecimal cost = BigDecimal.valueOf(0);
 
-        //add cost of each invoice-product -> invoice cost
+        //add cost of each invoice-product for each invoice and write it to invoice cost field
         for (InvoiceProductDTO each : invoiceProductListById) {
             BigDecimal currItemCost = each.getPrice().multiply(BigDecimal.valueOf(each.getQty()));
             cost = cost.add(currItemCost);
