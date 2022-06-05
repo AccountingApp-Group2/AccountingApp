219d6ca943f4f43de412b1598ef6d43303a35890
alexandrcorenciuc <alexandrcorenciuc@gmail.com>
2022-06-04 18:38:40 -0700

    some small changes

--- src/main/java/com/example/accountingapp/controller/ReportController.java
+++ src/main/java/com/example/accountingapp/controller/ReportController.java
@@ -1,7 +1,10 @@
 package com.example.accountingapp.controller;
 
 
+import com.example.accountingapp.service.ProfitLossService;
+import com.example.accountingapp.service.StockService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,17 +12,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/report")
 public class ReportController {
 
+    private final ProfitLossService profitLossService;
+    private final StockService stockService;
+
+    public ReportController(ProfitLossService profitLossService, StockService stockService) {
+        this.profitLossService = profitLossService;
+        this.stockService = stockService;
+    }
+
     @GetMapping("/stock")
-    public String clickStock(){
+    public String clickStock(Model model){
+
+        model.addAttribute("stocks", stockService.listAllStocks());
 
         return "/report/stock-report";
     }
 
-    @GetMapping("/profit")
-    public String clickProfit(){
 
-        return "/report/profit-loss-report";
-    }
+//    @GetMapping("/profit")
+//    public String clickProfit(){
+//
+//        return "/report/profit-loss-report";
+//    }
 }
 
 
