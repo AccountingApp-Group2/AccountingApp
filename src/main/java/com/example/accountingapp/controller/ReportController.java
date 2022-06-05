bd842173eb7cce11c9fdff76334c58be60c15740
alexandrcorenciuc <alexandrcorenciuc@gmail.com>
2022-06-05 12:24:42 -0700

    some small changes

--- src/main/java/com/example/accountingapp/controller/ReportController.java
+++ src/main/java/com/example/accountingapp/controller/ReportController.java
@@ -1,11 +1,14 @@
 package com.example.accountingapp.controller;
 
 
+import com.example.accountingapp.service.ProductService;
 import com.example.accountingapp.service.StockDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/report")
@@ -13,16 +16,19 @@ public class ReportController {
 
 
     private final StockDetailsService stockService;
+    private final ProductService productService;
 
-    public ReportController( StockDetailsService stockService) {
-
+    public ReportController(StockDetailsService stockService, ProductService productService) {
         this.stockService = stockService;
+        this.productService = productService;
     }
 
     @GetMapping("/stock")
     public String clickStock(Model model){
 
         model.addAttribute("stocks", stockService.listAllStocks());
+
+        model.addAttribute("products", productService.findById(stockService.listAllStocks().stream().map(product -> product.getId())));
 
         return "/report/stock-report";
     }
--- src/main/java/com/example/accountingapp/dto/StockDTO.java
+++ src/main/java/com/example/accountingapp/dto/StockDTO.java
@@ -1,23 +1,36 @@
 package com.example.accountingapp.dto;
 
+import com.example.accountingapp.entity.Product;
 import com.example.accountingapp.enums.InvoiceType;
 import com.example.accountingapp.enums.SalePurchaseStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class StockDTO {
-    
-    private LocalDateTime dateTime;
-    private String quantity;
-//    private InvoiceType invoiceType;
-    private int basePrice;
-    private SalePurchaseStatus salePurchaseStatus;
 
-    private String invoiceProductDTO;
+    private Long id;
+
+    @NotBlank
+    private LocalDateTime iDate;
+
+    @NotBlank
+    private BigInteger quantity;
+
+    @NotBlank
+    private BigDecimal price;
+
+    @NotBlank
+    private BigInteger remainingQuantity;
+
+
 }
