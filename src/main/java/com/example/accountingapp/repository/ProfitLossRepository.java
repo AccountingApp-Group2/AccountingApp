6dcf6dccccf0911156b0758067e40217f194fd71
RumCake01 <mia.hasnutdinova@gmail.com>
2022-06-05 14:45:31 -0400

    Update links, deleted ProfitLoss classes, ReportCotroller removed injection, renamed StockDetails to StockDetailsRepository. Created StockServiceDetailsImpl

--- src/main/java/com/example/accountingapp/repository/ProfitLossRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.accountingapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-//@Repository
-//public interface ProfitLossRepository extends JpaRepository {
-//    // ToDo find out what to pass on line 5
-//}
