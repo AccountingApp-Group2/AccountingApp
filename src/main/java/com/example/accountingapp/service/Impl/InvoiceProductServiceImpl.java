cb0bea9e098122165c4e2d1d37be32a7c950b087
BahaMaksudov <bmaksudov@gmail.com>
2022-06-23 22:55:42 -0400

    Crud operations Sales Invoice

--- src/main/java/com/example/accountingapp/service/Impl/InvoiceProductServiceImpl.java
+++ src/main/java/com/example/accountingapp/service/Impl/InvoiceProductServiceImpl.java
@@ -1,34 +1,36 @@
 package com.example.accountingapp.service.impl;
 
 
-import com.example.accountingapp.dto.CompanyDTO;
-import com.example.accountingapp.dto.InvoiceDTO;
 import com.example.accountingapp.dto.InvoiceProductDTO;
-import com.example.accountingapp.enums.InvoiceType;
-import com.example.accountingapp.mapper.MapperUtil;
-import com.example.accountingapp.repository.InvoiceRepository;
 import com.example.accountingapp.entity.InvoiceProduct;
+import com.example.accountingapp.mapper.MapperUtil;
 import com.example.accountingapp.repository.InvoiceProductRepository;
 import com.example.accountingapp.service.InvoiceProductService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
-
 
 @Service
 public class InvoiceProductServiceImpl implements InvoiceProductService {
-
     final private InvoiceProductRepository invoiceProductRepository;
+    private final MapperUtil mapperUtil;
 
-    public InvoiceProductServiceImpl(InvoiceProductRepository invoiceProductRepository) {
+    public InvoiceProductServiceImpl(InvoiceProductRepository invoiceProductRepository, MapperUtil mapperUtil) {
         this.invoiceProductRepository = invoiceProductRepository;
+        this.mapperUtil = mapperUtil;
     }
 
     @Override
     public List<InvoiceProduct> listAll() {
         return invoiceProductRepository.findAll();
+    }
+
+    @Override
+    public InvoiceProductDTO getByInvoiceId(Long invoiceId) {
+
+        InvoiceProductDTO invoiceProductDTO = mapperUtil.convert(invoiceProductRepository.getByInvoiceId(invoiceId),new InvoiceProductDTO());
+
+        return invoiceProductDTO;
     }
 
 }
