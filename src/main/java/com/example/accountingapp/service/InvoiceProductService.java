6687fdd3620fa31f9ac2389415c1ef76233020d5
VitalySlabada <vitaly.slabada@gmail.com>
2022-07-08 22:19:40 -0500

    cleanup, minor fixes

--- src/main/java/com/example/accountingapp/service/InvoiceProductService.java
+++ src/main/java/com/example/accountingapp/service/InvoiceProductService.java
@@ -2,16 +2,15 @@ package com.example.accountingapp.service;
 
 import com.example.accountingapp.dto.InvoiceProductDTO;
 import com.example.accountingapp.dto.ProductDTO;
-import com.example.accountingapp.dto.InvoiceProductDTO;
 import com.example.accountingapp.entity.InvoiceProduct;
-import com.example.accountingapp.enums.InvoiceType;
+
 import java.util.List;
 
 public interface InvoiceProductService {
 
     List<InvoiceProduct> listAll();
-    List<ProductDTO> findAllProductsByCompanyName(String companyName);
 
+    List<ProductDTO> findAllProductsByCompanyName(String companyName);
 
     void addInvoiceProductByInvoiceId(Long id, InvoiceProductDTO invoiceProductDTO);
 
@@ -20,7 +19,6 @@ public interface InvoiceProductService {
     Long findInvoiceIdByInvoiceProductId(Long ipid);
 
     void deleteInvoiceProductById(Long ipid);
-
 
     List<InvoiceProductDTO> getByInvoiceId(Long invoiceId);
 
